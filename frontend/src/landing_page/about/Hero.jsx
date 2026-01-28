fd34cb27901f58ba7f30fc83a85e444bb00489cf
discreetso <visualcrn.001@gmail.com>
2026-01-28 21:01:09 +0500

    Update company details for TradFi in Hero.jsx

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -12,20 +12,20 @@ function Hero() {
             style={{ lineHeight: '1.3', fontSize: '1.2em'}}>
                 <div className="col-6 p-5">
                     <p>
-                        We kick-started operations on the 15th of August, 2010 with the goal of 
-                        breaking all barriers that traders and investors face in India in terms of cost, 
-                        support, and technology. We named the company Zerodha, a combination of Zero and 
-                        "Rodha", the Sanskrit word for barrier.
+                        We kick-started operations on the 15th of August, 2025 with the goal of 
+                        breaking all barriers that traders and investors face in Pakistan in terms of cost, 
+                        support, and technology. We named the company TradFi, a combination of Trading and 
+                        Finance.
                     </p>
 
                     <p>
                         Today, our disruptive pricing models and in-house technology have made us the 
-                        biggest stock broker in India.
+                        biggest stock broker in Pakistan.
                     </p>
 
                     <p>
                         Over 1+ Crore clients place millions of orders every day through our powerful 
-                        ecosystem of investment platforms, contributing over 15% of all Indian retail 
+                        ecosystem of investment platforms, contributing over 15% of all Pakistan retail 
                         trading volumes.
                     </p>
                 </div>
@@ -39,7 +39,7 @@ function Hero() {
                     <p>
                         <a href="https://www.rainmatter.com" style={{ textDecoration:'none' }}>Rainmatter</a>, 
                         our fintech fund and incubator, has invested in several fintech startups with 
-                        the goal of growing the Indian capital markets. 
+                        the goal of growing the Pakistan capital markets. 
                     </p>
 
                     <p>
@@ -48,7 +48,7 @@ function Hero() {
                         In addition, we run a number of popular open online educational and community
                         initiatives to empower retail traders and investors. Rainmatter, our fintech
                         fund and incubator, has invested in several fintech startups with the goal of
-                        growing the Indian capital markets. And yet, we are always up to something
+                        growing the Pakistan capital markets. And yet, we are always up to something
                         new every day. Catch up on the latest updates on our blog or see what the
                         media is saying.
                     </p>
