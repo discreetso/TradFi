3546cca46af231798f530902ff0c20e3f64ce21f
discreetso <visualcrn.001@gmail.com>
2026-01-28 21:03:53 +0500

    update

--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -9,24 +9,24 @@ function Team() {
             style={{ lineHeight: '1.3', fontSize: '1.2em'}}>
                 <div className="col-6 p-5 text-center">
                     <img src="media/images/nithinKamath.jpg" alt="OwnerImage" style={{ borderRadius:'100%', width:'50%' }} />
-                    <h4 className="mt-5">Nithin Kamath</h4>
+                    <h4 className="mt-5">John Doe</h4>
                     <h6>Founder, CEO</h6>
                 </div>
                     
                 <div className="col-6 p-3">
                     <p>
-                        Nithin Kamath is an Indian entrepreneur, best known as the
-                        co-founder and CEO of Zerodha, India's largest stock brokerage firm. 
-                        He is recognized for revolutionizing the Indian brokerage industry by 
+                        John Doe is Pakistani entrepreneur, best known as the
+                        co-founder and CEO of TradFi, Pakistan's largest stock brokerage firm. 
+                        He is recognized for revolutionizing the Pakistani brokerage industry by 
                         pioneering a low-cost, flat-fee model for trading. 
                     </p>
                     <p>
-                        Kamath began his career in the stock market at the age of 17 and was an
-                        active trader for over a decade before founding Zerodha. He worked 
+                        John began his career in the stock market at the age of 17 and was an
+                        active trader for over a decade before founding TradFi. He worked 
                         night shifts at a call center to fund his daytime trading in his early years.
                     </p>
                     <p>
-                        He was named EY Entrepreneur Of The Year 2024 for his contributions to 
+                        He was named Entrepreneur Of The Year 2024 for his contributions to 
                         financial services industry
                     </p>
                     <p>
